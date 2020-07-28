219365945becb5c668241eab534d1aaad8d4385d
Sowmya Nelakurthi <sonelaku@in.ibm.com>
2020-07-28 13:22:05 +0530

    Add project files.

---
+++ MySite/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using MySite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult Index(MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.To.Add("nelakurthisowmya123@gmail.com");
+                mail.From = new MailAddress("nelakurthisowmya123@gmail.com");
+
+                mail.Subject = "Thanks for your Valuable FeedBack" + _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("nelakurthisowmya123@gmail.com", "Sowmya@12345"); // Enter seders User name and password  
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("About");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult About()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ MySite/Models/MailModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MySite.Models
+{
+    public class MailModel
+    {
+
+        [Required(ErrorMessage = "Email is required.")]
+        // [E(ErrorMessage = "Invalid Email Address.")]
+        public string To
+        {
+            get;
+            set;
+        }
+        public string Subject
+        {
+            get;
+            set;
+        }
+        public string Body
+        {
+            get;
+            set;
+        }
+    }
+}
